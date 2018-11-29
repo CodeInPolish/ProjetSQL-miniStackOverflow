155320a4e0dae1758b4adbed3c60ecc0c2ddc3e3
Marcin Krasowski <marcin.krasowski@hotmail.fr>
2018-11-29 15:39:20 +0100

    Auto stash before merge of "master" and "origin/master"

--- DATA.sql
+++ DATA.sql
@@ -9,7 +9,19 @@ UNION (SELECT qt.date, qt.question_id, qt.user_id, qt.edit_date, qt.edit_user, q
 
 /**
 
-Insert users
+Insert users (email,pseudo,password)
 
 **/
-SELECT ProjetSQL.createUser('','','');
+/**
+SELECT ProjetSQL.createUser('123@gmail.com','jeanjak','jeanjak');
+SELECT ProjetSQL.createUser('1@gmail.com','jeanbul','jeanbul');
+SELECT ProjetSQL.createUser('juju@gmail.com','buzz','buzz');
+SELECT ProjetSQL.createUser('mamen@gmail.com','slope','slope');
+SELECT ProjetSQL.createUser('hardbass@gmail.com','Squat','squat');
+SELECT ProjetSQL.createUser('bak@gmail.com','jakjanu','jakjanu');
+SELECT ProjetSQL.createUser('mam@gmail.com','slope','slop');
+SELECT ProjetSQL.createUser('hardubass@gmail.com','Squat','uatya');
+SELECT ProjetSQL.createUser('baluk@gmail.com','jakjanu','boozlok');
+**/
+
+
